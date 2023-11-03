0641ab1875259ed816ebced751ddd75ab16c9750
BrayanCortes123 <lolhola189@gmail.com>
2023-11-03 10:44:45 -0500

    asddf

--- dodgeandgo/js/Rock.js
+++ dodgeandgo/js/Rock.js
@@ -1,60 +1,46 @@
 let obstacle1 = document.getElementById("obstacle1");
 let obstacle2 = document.getElementById("obstacle2");
 
-function moveObstacle(obstacle) {
-    let left = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+function moveObstacle1() {
+    let left = parseInt(window.getComputedStyle(obstacle1).getPropertyValue("left"));
     if (left <= 0) {
-        obstacle.style.left = "100vw";
+        obstacle1.style.left = "100vw";
     } else {
-        obstacle.style.left = left - 2 + "px";
+        obstacle1.style.left = left - 2 + "px";
     }
 }
 
-function checkCollision(obstacle) {
-    let bicyRect = mainbicy.getBoundingClientRect();
+function moveObstacle2() {
+    let left = parseInt(window.getComputedStyle(obstacle2).getPropertyValue("left"));
+    if (left <= 5) {
+        obstacle2.style.left = "100vw";
+    } else {
+        obstacle2.style.left = left - 2 + "px";
+    }
+}
+
+function checkCollision(obstacle, bicy) {
+    let bicyRect = bicy.getBoundingClientRect();
     let obstacleRect = obstacle.getBoundingClientRect();
 
     if (bicyRect.x < obstacleRect.x + obstacleRect.width &&
         bicyRect.x + bicyRect.width > obstacleRect.x &&
         bicyRect.y < obstacleRect.y + obstacleRect.height &&
         bicyRect.y + bicyRect.height > obstacleRect.y) {
-        alert("Game Over");
+      /*   alert("Game Over"); */
     }
 }
 
 setInterval(function() {
-    moveObstacle(obstacle1);
-    moveObstacle(obstacle2);
-    checkCollision(obstacle1);
-    checkCollision(obstacle2);
+    moveObstacle1();
+    checkCollision(obstacle1, mainbicy);
+    checkCollision(obstacle1, mainbicyTwo);
 }, 20);
 
-function moveObstacle(obstacle) {
-    let left = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
-    if (left <= 0) {
-        obstacle.style.left = "100vw";
-    } else {
-        obstacle.style.left = left - 2 + "px";
-    }
-}
-
-// Bicicleta 2
-
-function checkCollision(obstacle) {
-    let bicyRect = mainbicyTwo.getBoundingClientRect();
-    let obstacleRect = obstacle.getBoundingClientRect();
-
-    if (bicyRect.x < obstacleRect.x + obstacleRect.width &&
-        bicyRect.x + bicyRect.width > obstacleRect.x &&
-        bicyRect.y < obstacleRect.y + obstacleRect.height &&
-        bicyRect.y + bicyRect.height > obstacleRect.y) {
-        alert("Game Over");
-    }
-}
-
-setInterval(function() {
-    moveObstacle(obstacle1);
-    moveObstacle(obstacle2);
-    checkCollision(obstacle1);
-    checkCollision(obstacle2);
-}, 20);
+setTimeout(function() {
+    setInterval(function() {
+        moveObstacle2();
+        checkCollision(obstacle2, mainbicy);
+        checkCollision(obstacle2, mainbicyTwo);
+    }, 20);
+}, 8000);
